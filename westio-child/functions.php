d7182f191193e5296a93084522c569777b2e8a60
Victor <victor@Victors-MacBook-Air.local>
2026-07-29 15:41:52 +0400

    Add Google Tag Manager (GTM-MVK8Z6ST) via wp_head/wp_body_open

--- westio-child/functions.php
+++ westio-child/functions.php
@@ -83,3 +83,25 @@ add_action('wp_enqueue_scripts', function () {
 
 // Header CTA / phone are now edited per-language in the Header & Footer
 // admin page (see inc/footer-settings.php), no Polylang string scan needed.
+
+// Google Tag Manager.
+add_action('wp_head', function () {
+    ?>
+<!-- Google Tag Manager -->
+<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-MVK8Z6ST');</script>
+<!-- End Google Tag Manager -->
+    <?php
+}, 1);
+
+add_action('wp_body_open', function () {
+    ?>
+<!-- Google Tag Manager (noscript) -->
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-MVK8Z6ST"
+height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+<!-- End Google Tag Manager (noscript) -->
+    <?php
+});
